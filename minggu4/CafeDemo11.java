d6544c2a80ed812072a37b7285156efd790cc23c
DinaKSari <dinakumala7070@gmail.com>
2025-03-04 09:44:39 +0700

    QUIZ 2

--- minggu4/CafeDemo11.java
+++ minggu4/CafeDemo11.java
@@ -7,14 +7,18 @@ public class CafeDemo11{
     public static void main(String[] args) {
         Scanner dina11 = new Scanner(System.in);
         int pelanggan;
+        //memasukan jumlah pelanggan
         System.out.println("Masukan jumlah pelanggan : ");
         pelanggan = dina11.nextInt();
+        //membuat array of object
         Cafe11[] arrayOfPelanggan = new Cafe11[pelanggan];
         for(int i= 0; i<pelanggan;i++){
+            //membuat objek
             arrayOfPelanggan[i] = new Cafe11();
             System.out.println("Nama Pelanggan ke-" +(i+1)+ " : ");
             arrayOfPelanggan[i].nama = dina11.next();
             dina11.nextLine();
+            //menampilkan menu makanan dan minuman
             arrayOfPelanggan[i].menuKonsumsi();
             System.out.println("-- Masukan makanan/minuman yang ingin dibeli --");
             arrayOfPelanggan[i].makanan = dina11.next();
