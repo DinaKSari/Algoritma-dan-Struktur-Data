5d4658db193932043be0037d71aa3a606c3187fe
DinaKSari <dinakumala7070@gmail.com>
2025-03-18 09:05:15 +0700

    insertion sort

--- minggu6/SortingMain11.java
+++ minggu6/SortingMain11.java
@@ -2,8 +2,10 @@ public class SortingMain11 {
     public static void main(String[] args) {
         int a[] = {20,20,2,7,12};
         int b[] = {30,20,2,8,14};
+        int c[] = {40,10,4,9,3};
         Sorting11 dataUrut1 = new Sorting11(a, a.length);
         Sorting11 dataUrut2 = new Sorting11(b, b.length);
+        Sorting11 dataUrut3 = new Sorting11(c, c.length);
         System.out.println("Data awal 1");
         dataUrut1.tampil();
         dataUrut1.bubbleSort();
@@ -15,5 +17,11 @@ public class SortingMain11 {
         dataUrut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)"); 
         dataUrut2.tampil();
+
+        System.out.println("Data awal 3");
+        dataUrut3.tampil();
+        dataUrut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataUrut3.tampil();
     }
 }
