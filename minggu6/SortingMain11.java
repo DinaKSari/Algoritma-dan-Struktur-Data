d8c348fa9b3db542f1ef8424852b17739359bea5
DinaKSari <dinakumala7070@gmail.com>
2025-03-18 08:49:12 +0700

    selection sort

--- minggu6/SortingMain11.java
+++ minggu6/SortingMain11.java
@@ -1,12 +1,19 @@
 public class SortingMain11 {
     public static void main(String[] args) {
         int a[] = {20,20,2,7,12};
+        int b[] = {30,20,2,8,14};
         Sorting11 dataUrut1 = new Sorting11(a, a.length);
-
+        Sorting11 dataUrut2 = new Sorting11(b, b.length);
         System.out.println("Data awal 1");
         dataUrut1.tampil();
         dataUrut1.bubbleSort();
         System.out.println( "Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataUrut1.tampil();
+
+        System.out.println("Data awal 2");
+        dataUrut2.tampil();
+        dataUrut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)"); 
+        dataUrut2.tampil();
     }
 }
