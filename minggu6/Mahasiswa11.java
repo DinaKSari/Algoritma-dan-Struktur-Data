2e4dd66304df765e4121af840afe071e4e559801
DinaKSari <dinakumala7070@gmail.com>
2025-03-18 09:30:34 +0700

    Praktikum 2

---
+++ minggu6/Mahasiswa11.java
@@ -0,0 +1,24 @@
+public class Mahasiswa11 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+    // Konstruktor default
+    Mahasiswa11 () {
+    }
+    // Konstruktor berparameter (dibuat ada yang nama var parameter inputnya sama ada yang tidak)
+    Mahasiswa11 (String nm, String name, String kls, double ip) {
+    nim = nm;
+    nama = name;
+    ipk = ip;
+    kelas = kls;
+    }
+
+    void tampilinformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+}
+
