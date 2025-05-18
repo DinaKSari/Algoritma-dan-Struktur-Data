c048a191dc9ce001cc34fe50a1c5b563633b20a9
DinaKSari <dinakumala7070@gmail.com>
2025-05-18 19:59:19 +0700

    Membuat class

---
+++ P1Jobsheet10/Pertemuan12/Mahasiswa11.java
@@ -0,0 +1,15 @@
+public class Mahasiswa11 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    Mahasiswa11(){}
+    Mahasiswa11(String nm, String name, String kls, double ip){
+
+    }
+
+    void tampilInformasi(){
+        
+    }
+}
