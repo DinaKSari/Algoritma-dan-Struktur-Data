974d73db56cf06f180e5c371545870c4eb377df8
DinaKSari <dinakumala7070@gmail.com>
2025-05-18 20:04:51 +0700

    method print

--- P1Jobsheet10/Pertemuan12/SingleLinkedList11.java
+++ P1Jobsheet10/Pertemuan12/SingleLinkedList11.java
@@ -5,4 +5,18 @@ public class SingleLinkedList11 {
     boolean isEmpty(){
         return (head == null);
     }
+
+    public void print() {
+        if (!isEmpty()) {
+        Node11 tmp = head;
+        System.out.print( "Isi Linked List:\t");
+        while (tmp != null) {
+        tmp.data.tampilInformasi();
+        tmp = tmp.next;
+        }
+        System.out.println("");
+        } else {
+        System.out.println( "Linked list kosong");
+        }
+    }
 }
