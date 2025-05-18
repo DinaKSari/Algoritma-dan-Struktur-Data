23d4b7b18e10aa496da1ea7cbb78a96d8e4ee1b9
DinaKSari <dinakumala7070@gmail.com>
2025-05-18 21:49:44 +0700

    Praktikum 2, menambah method di singlelinkedlist

--- P1Jobsheet10/Pertemuan12/SLLMain11.java
+++ P1Jobsheet10/Pertemuan12/SLLMain11.java
@@ -1,11 +1,39 @@
+import java.util.Scanner;
 public class SLLMain11 {
     public static void main(String[] args) {
+        //nambah scanner untuk input soal no.3
+        Scanner dina11 = new Scanner(System.in);
         SingleLinkedList11 sll = new SingleLinkedList11();
+        /* ini data yang ada sebelumnya.
         Mahasiswa11 mhs1 = new Mahasiswa11("2001","Dono","1A", 3.5);
         Mahasiswa11 mhs2 = new Mahasiswa11("2002","Sylvia","1A", 3.6);
         Mahasiswa11 mhs3 = new Mahasiswa11("2003","Xavier","1A", 3.5);
         Mahasiswa11 mhs4 = new Mahasiswa11("2004","Caulus","1D", 3.3);
+        */
 
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlah = dina11.nextInt();
+        dina11.nextLine();
+
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("\nData Mahasiswa ke-" + (i+1));
+            System.out.print("NIM     : ");
+            String nim = dina11.nextLine();
+            System.out.print("Nama    : ");
+            String nama = dina11.nextLine();
+            System.out.print("Kelas   : ");
+            String kelas = dina11.nextLine();
+            System.out.print("IPK     : ");
+            double ipk = dina11.nextDouble();
+            dina11.nextLine();
+
+            Mahasiswa11 mhs = new Mahasiswa11(nim, nama, kelas, ipk);
+            sll.addLast(mhs); 
+        }
+
+        System.out.println("\nData seluruh mahasiswa dalam linked list:");
+        sll.print();
+        /* ini kode sebelum modif agar bisa input pakai keyboard
         sll.print();
         sll.addFirst(mhs4);
         sll.print();
@@ -13,6 +41,6 @@ public class SLLMain11 {
         sll.print();
         sll.insertAfter ( "Caulus", mhs3);
         sll.insertAt( 2, mhs2);
-        sll.print();
+        sll.print();*/
     }    
 }
