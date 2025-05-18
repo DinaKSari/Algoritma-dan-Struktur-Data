94dcaee741c34031e9a951c71a0da8a750961b3b
DinaKSari <dinakumala7070@gmail.com>
2025-05-18 20:25:32 +0700

    menambahkan objek di main

--- P1Jobsheet10/Pertemuan12/SLLMain11.java
+++ P1Jobsheet10/Pertemuan12/SLLMain11.java
@@ -1,3 +1,11 @@
 public class SLLMain11 {
-    
+    public static void main(String[] args) {
+        SingleLinkedList11 list = new SingleLinkedList11();
+        Mahasiswa11 mhs1 = new Mahasiswa11("2001","Dono","1A", 3.5);
+        Mahasiswa11 mhs2 = new Mahasiswa11("2002","Sylvia","1A", 3.6);
+        Mahasiswa11 mhs3 = new Mahasiswa11("2003","Xavier","1A", 3.5);
+        Mahasiswa11 mhs4 = new Mahasiswa11("2004","Caulus","1D", 3.3);
+
+
+    }    
 }
