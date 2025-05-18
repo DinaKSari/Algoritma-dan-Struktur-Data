49671c2662bf861a8df2cd2f752c2e2819c9149d
DinaKSari <dinakumala7070@gmail.com>
2025-05-18 20:27:14 +0700

    menggunaka method penambahan data dan menampilkan data

--- P1Jobsheet10/Pertemuan12/SLLMain11.java
+++ P1Jobsheet10/Pertemuan12/SLLMain11.java
@@ -1,11 +1,18 @@
 public class SLLMain11 {
     public static void main(String[] args) {
-        SingleLinkedList11 list = new SingleLinkedList11();
+        SingleLinkedList11 sll = new SingleLinkedList11();
         Mahasiswa11 mhs1 = new Mahasiswa11("2001","Dono","1A", 3.5);
         Mahasiswa11 mhs2 = new Mahasiswa11("2002","Sylvia","1A", 3.6);
         Mahasiswa11 mhs3 = new Mahasiswa11("2003","Xavier","1A", 3.5);
         Mahasiswa11 mhs4 = new Mahasiswa11("2004","Caulus","1D", 3.3);
 
-
+        sll.print();
+        sll.addFirst(mhs4);
+        sll.print();
+        sll.addLast(mhs1);
+        sll.print();
+        sll.insertAfter ( "Dirga", mhs3);
+        sll.insertAt( 2, mhs2);
+        sll.print();
     }    
 }
