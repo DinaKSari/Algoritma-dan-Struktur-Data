10da11758c5d32e0b13f857da25223eb00964797
DinaKSari <dinakumala7070@gmail.com>
2025-05-19 18:13:02 +0700

    Membuat class mahasiswa11

---
+++ P1Jobsheet10/Pertemuan12/TugasPertemuan12/Mahasiswa11.java
@@ -0,0 +1,15 @@
+package TugasPertemuan12;
+
+public class Mahasiswa11 {
+    String nim;
+    String nama;
+    String prodi;
+    String kelas;
+
+    Mahasiswa11(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+}
---
+++ TugasPertemuan12/NodeAkademik11.java
@@ -0,0 +1,9 @@
+public class NodeAkademik11{
+    MahasiswaAkademik11 data;
+    NodeAkademik11 next;
+
+    public NodeAkademik11(MahasiswaAkademik11 data, NodeAkademik11 next){
+        this.data = data;
+        this.next = next;
+    }
+}
