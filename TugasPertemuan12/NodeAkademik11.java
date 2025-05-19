752539e1bd38f09aa9b6edab63eef36d4f5f755b
DinaKSari <dinakumala7070@gmail.com>
2025-05-19 19:31:39 +0700

    melihat error

--- TugasPertemuan12/NodeAkademik11.java
+++
@@ -1,9 +0,0 @@
-public class NodeAkademik11{
-    MahasiswaAkademik11 data;
-    NodeAkademik11 next;
-
-    public NodeAkademik11(MahasiswaAkademik11 data, NodeAkademik11 next){
-        this.data = data;
-        this.next = next;
-    }
-}
