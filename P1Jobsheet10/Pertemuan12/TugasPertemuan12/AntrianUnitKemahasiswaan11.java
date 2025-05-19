49c848cf5455725a39bfc3e02dcc055850773c76
DinaKSari <dinakumala7070@gmail.com>
2025-05-19 18:39:50 +0700

    Membuat menu dan pilihan menu

--- P1Jobsheet10/Pertemuan12/TugasPertemuan12/AntrianUnitKemahasiswaan11.java
+++ P1Jobsheet10/Pertemuan12/TugasPertemuan12/AntrianUnitKemahasiswaan11.java
@@ -5,6 +5,32 @@ public class AntrianUnitKemahasiswaan11 {
         QueueLinkedList11 queue = new QueueLinkedList11(5);
         int pilihan;
 
-        
+        do {
+            System.out.println("\n=== Menu Antrian Unit Kemahasiswaan ===");
+            System.out.println("1. Tambah Mahasiswa ke Antrian");
+            System.out.println("2. Layani Mahasiswa");
+            System.out.println("3. Lihat Mahasiswa Terdepan");
+            System.out.println("4. Lihat Mahasiswa Terakhir");
+            System.out.println("5. Lihat Semua Antrian");
+            System.out.println("6. Jumlah Antrian");
+            System.out.println("7. Kosongkan Antrian");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = dina11.nextInt(); dina11.nextLine();
+
+            switch (pilihan){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 0:
+            default:
+        }
+        } while (pilihan != 0);
+
+        dina11.close();
     }
 }
