7354ffcb8e656a56ef866ede51350bf1c1f13fd5
DinaKSari <dinakumala7070@gmail.com>
2025-05-19 18:36:41 +0700

    membuat class AntrianUnitKemahasiswaan11, lalu variable yang ada dan objek di dalamnya

---
+++ P1Jobsheet10/Pertemuan12/TugasPertemuan12/AntrianUnitKemahasiswaan11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class AntrianUnitKemahasiswaan11 {
+    public static void main(String[] args) {
+        Scanner dina11 = new Scanner(System.in);
+        QueueLinkedList11 queue = new QueueLinkedList11(5);
+        int pilihan;
+
+        
+    }
+}
