0b040bdca75e69f4512678cf3f290f8bc423bb65
DinaKSari <dinakumala7070@gmail.com>
2025-05-20 11:05:40 +0700

    membuat package vasinasi dan class nya

--- minggu13/vaksinasi/Node11.java
+++ minggu13/vaksinasi/Node11.java
@@ -1,5 +1,12 @@
 package vaksinasi;
 
 public class Node11 {
-    
+    int data;
+    Node11 prev, next;
+
+    Node11(Node11 prev, int data, Node11 next){
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
 }
